36e128ea1bce76f54da11d2a09aaefb8adebd44b
박기림 <59854126+GirimPark@users.noreply.github.com>
2023-03-20 16:56:18 +0900

    개인정보 수집 유효기간

--- Programmers/개인정보 수집 유효기간.cpp
+++ Programmers/개인정보 수집 유효기간.cpp
@@ -17,10 +17,15 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
     for (string s : privacies)
         new_privacies.push_back({s.substr(11, 1), s.substr(0, 10)});
     
+    // 현재 날짜 int형 변수로 받기
+    int curYear = stoi(today.substr(0, 4));
+    int curMonth = stoi(today.substr(5, 2));
+    int curDay = stoi(today.substr(8, 2));
+
     string type;
     int term, year, month, day;
     string expiration;
-
+    int exYear=0, exMonth=0, exDay=0;
     for (int i = 0; i < new_privacies.size(); i++)
     {
         type = new_privacies[i][0];
@@ -31,10 +36,37 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
             month = stoi(new_privacies[i][1].substr(5, 2));
             day = stoi(new_privacies[i][1].substr(8, 2));
 
-            if (month + term < 12 && day > 1)    expiration = to_string(year) + "." + to_string(month + term) + "." + to_string(day - 1);
-            else if (month + term < 13 && day == 1) expiration = to_string(year) + "." + to_string(month + term - 1) + ".28";
-            else if (month)
-            
+            if ((month + term) < 13 && day > 1)   // year, month 넘어가지 않음
+            {
+                exYear = year; 
+                exMonth = month + term; 
+                exDay = day - 1;
+            }
+            else if ((month + term) < 13 && day == 1) // month 넘어감
+            {
+                exYear = year; 
+                exMonth = month + term - 1; 
+                exDay = 28;
+            }
+            else if ((month + term) > 12 && day > 1)  // year 넘어감
+            {
+                exYear = year + (month + term) / 12; 
+                exMonth = month + term - 12 * ((month + term) / 12); 
+                exDay = day - 1;
+            }
+            else if ((month + term) > 12 && day == 1) // year, day 넘어감
+            {
+                exYear = year + (month + term) / 12; 
+                exMonth = month + term - 12 * ((month + term) / 12) - 1; 
+                exDay = 28;
+            }
+
+            if (exYear < curYear)
+                answer.push_back(i + 1);
+            else if (exYear == curYear && exMonth < curMonth)
+                answer.push_back(i + 1);
+            else if (exYear == curYear && exMonth == curMonth && exDay < curDay)
+                answer.push_back(i + 1);
         }
     }
     
@@ -43,13 +75,14 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
 
 int main()
 {
-    string today = "2022.05.19";
-    vector<string> terms = { "A 6", "B 12", "C 3" };
-    vector<string> privacies = { "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C" };
+    string today = "2021.12.08";
+    vector<string> terms = { "A 18" };
+    vector<string> privacies = { "2020.06.08 A" };
+    // 만료일 : 2020.24.08 2021.12.08
 
     for (int a : solution(today, terms, privacies))
     {
-        //cout << a << " ";
+        cout << a << " ";
     }
     
     return 0;
